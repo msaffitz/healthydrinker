1eab6d546725a6f8f86f13bd77c47fded0e4bc6d
Sky Kelsey <sky.kelsey@gmail.com>
2012-04-14 13:57:15 -0700

    Refactor remove the require stuff since I don't have time to learn it.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 require.config({
   baseUrl: "./"
 });
-require(["lib/jquery-1.7.2", "lib/jquery.mobile-1.1.0"], function(util) {
+require(["lib/jquery-1.7.2", "lib/jquery.mobile-1.1.0", "src/icons"], function(util) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
