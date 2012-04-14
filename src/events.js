7bb411a999a6a92236b880ad36845d3a3d708261
Michael Saffitz <m@saffitz.com>
2012-04-14 15:34:37 -0700

    navbar

--- src/events.js
+++ src/events.js
@@ -1,5 +1,5 @@
 var mHealthToken = '';
-$('[data-role=page]').live('pageshow', function( ev, data) {
+$('[data-role=page]#selection').live('pageshow', function( ev, data) {
   mHealthToken = window.location.hash.match("=.*")[0].substring(1);
   $.mobile.changePage($('#selection'));
 });
