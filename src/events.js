bcaeecb178713c5d3a3f9b4cd8d53c80a7acf47e
Michael Saffitz <m@saffitz.com>
2012-04-14 17:34:15 -0700

    remove events

--- src/events.js
+++
@@ -1,5 +0,0 @@
-var mHealthToken = '';
-$('[data-role=page]#selection').live('pageshow', function( ev, data) {
-  mHealthToken = window.location.hash.match("=.*")[0].substring(1);
-  $.mobile.changePage($('#selection'));
-});
