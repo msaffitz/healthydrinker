0cc5fcbb3ab7540e485e5946bfa0d739cd11fae6
Michael Saffitz <m@saffitz.com>
2012-04-14 19:13:30 -0700

    steps in seperate chart

--- cheers.rb
+++ cheers.rb
@@ -44,8 +44,35 @@ get '/getdata' do
   stepsO[:data] = stepsO[:data].map { |k,v| [k,v] }
 
   content_type :json
-  [drinksO, commitsO, stepsO].to_json
+  [drinksO, commitsO].to_json
 end
+
+get '/getdata2' do
+  logger.info "token is #{session[:access_token]}"
+  drinksR = RestClient.get("https://api-mhealth.att.com/v2/health/source/healthydrinker/data?oauth_token=#{session[:access_token]}")
+  changesR = RestClient.get("https://api-mhealth.att.com/v2/health/data?m=gitlogger/changed_lines&oauth_token=#{session[:access_token]}")
+  stepsR = RestClient.get("https://api-mhealth.att.com/v2/health/data?m=fitbit/steps&oauth_token=#{session[:access_token]}")
+
+  drinks = JSON.parse(drinksR)
+  commits = JSON.parse(changesR)
+  steps = JSON.parse(stepsR)
+
+  drinksO = {label: "Drinks", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}}
+  commitsO = {label: "Changes", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}, yaxis: 2}
+  stepsO = {label: "Steps", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}, yaxis: 3}
+
+  drinks.map { |r|  drinksO[:data][Time.parse( r["timestamp"] ).wday.to_s] += r["value"].to_i }
+  commits.map { |r| commitsO[:data][Time.parse( r["timestamp"] ).wday.to_s] +=  r["value"].to_i  }
+  steps.map { |r| stepsO[:data][Time.parse( r["timestamp"] ).wday.to_s] +=  r["value"]  }
+
+  drinksO[:data] = drinksO[:data].map { |k,v| [k,v] }
+  commitsO[:data] = commitsO[:data].map { |k,v| [k,v] }
+  stepsO[:data] = stepsO[:data].map { |k,v| [k,v] }
+
+  content_type :json
+  [drinksO, stepsO].to_json
+end
+
 
 
 get '/callback' do
