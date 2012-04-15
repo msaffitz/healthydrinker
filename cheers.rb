787374ccb8f2f15b77b3e8e299f4ad91b45bea25
Michael Saffitz <m@saffitz.com>
2012-04-14 19:01:36 -0700

    2 y

--- cheers.rb
+++ cheers.rb
@@ -32,7 +32,7 @@ get '/getdata' do
   # steps = JSON.parse(stepsR)
 
   drinksO = {label: "Drinks", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}}
-  commitsO = {label: "Changes", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}}
+  commitsO = {label: "Changes", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}, yaxis: 2}
   stepsO = {label: "Steps", data: {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0}}
 
   drinks.map { |r|  drinksO[:data][Time.parse( r["timestamp"] ).wday.to_s] += r["value"].to_i }
